9c4291b872a1e49b0bba2ebf8e36bc069ff2b3db
Beatriz Oliveira <beafatec@gmail.com>
2024-03-26 11:51:54 -0300

    comitt

---
+++ src/view/Principal.java
@@ -0,0 +1,12 @@
+// git@github.com:01Trixie/SemaforoLista05_Ex1.git
+
+package view;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
