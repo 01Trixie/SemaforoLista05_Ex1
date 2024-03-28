7ab92877ef133db98802e3eb1e08b3f6d1b85d5a
Beatriz Oliveira <161655658+01Trixie@users.noreply.github.com>
2024-03-28 11:42:39 -0300

    Update Cruzamento.java

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,11 +1,19 @@
 // git@github.com:01Trixie/SemaforoLista05_Ex1.git
+import java.util.concurrent.Semaphore;
+import controller.CruzamentoController;
 
 package view;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String sentido[] = {"Norte", "Sul", "Leste", "Oeste"};
+		int permissao1 = 1;
+		Semaphore semaforo1 = new Semaphore(permissao1);
+		for(int idCarro = 0; idCarro < 4; idCarro++) {
+			Thread tcruza = new CruzamentoController(idCarro, sentido, semaforo1);
+			tcruza.start();
+		}
 
 	}
 
